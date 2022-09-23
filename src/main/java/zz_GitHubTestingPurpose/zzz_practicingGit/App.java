b965425088458f941acf099fc228add448bf2ed1
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2022-09-23 21:44:37 +0530

    23_sep_22 commiting

---
+++ src/main/java/zz_GitHubTestingPurpose/zzz_practicingGit/App.java
@@ -0,0 +1,13 @@
+package zz_GitHubTestingPurpose.zzz_practicingGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
