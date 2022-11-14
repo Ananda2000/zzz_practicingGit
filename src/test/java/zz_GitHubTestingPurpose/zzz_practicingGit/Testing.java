2699635e4945ace1b649d16b79bdcbbac20aca41
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2022-11-14 22:30:17 +0530

    pushing to branch_1 new created

--- src/test/java/zz_GitHubTestingPurpose/zzz_practicingGit/Testing.java
+++ src/test/java/zz_GitHubTestingPurpose/zzz_practicingGit/Testing.java
@@ -7,5 +7,7 @@ public class Testing {
 		System.out.println("This is for git testing....");
 		System.out.println("this is updted now");
 	System.out.println("this one edited in github to check pull request..");
+	
+	System.out.println("This for creating the branch... and new branch..");
 	}
 }
