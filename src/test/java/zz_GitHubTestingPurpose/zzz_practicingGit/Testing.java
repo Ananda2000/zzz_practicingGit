df4f7778cd9aa968ce45d4c629e39e0aa2ecfe7c
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2022-11-14 08:29:22 +0530

    this is 1st update

---
+++ src/test/java/zz_GitHubTestingPurpose/zzz_practicingGit/Testing.java
@@ -0,0 +1,10 @@
+package zz_GitHubTestingPurpose.zzz_practicingGit;
+
+public class Testing {
+
+	
+	public static void main(String[] args) {
+		System.out.println("This is for git testing....");
+		System.out.println("this is updted now");
+	}
+}
