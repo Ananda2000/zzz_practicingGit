049a80ddf977b20bb059c38293f323bc051077c2
Ananda2000 <41889321+Ananda2000@users.noreply.github.com>
2022-11-14 22:08:24 +0530

    Update Testing.java

--- src/test/java/zz_GitHubTestingPurpose/zzz_practicingGit/Testing.java
+++ src/test/java/zz_GitHubTestingPurpose/zzz_practicingGit/Testing.java
@@ -6,5 +6,6 @@ public class Testing {
 	public static void main(String[] args) {
 		System.out.println("This is for git testing....");
 		System.out.println("this is updted now");
+	System.out.println("this one edited in github to check pull request..");
 	}
 }
